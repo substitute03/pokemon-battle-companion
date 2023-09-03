e160b34f5423a0cf3c497c8e647a00299cdf648b
Tom Naylor <substitute03@hotmail.com>
2023-09-03 17:02:24 +0100

    Recolour frozen cells.

--- src/app/type-chart/type-chart.component.ts
+++ src/app/type-chart/type-chart.component.ts
@@ -68,24 +68,52 @@ export class TypeChartComponent implements OnInit {
     public freezeRow(r: number): void {
         if (this.frozenRowIndex !== r && this.frozenRowIndex === -1) {
             this.frozenRowIndex = r;
+
+            this.typeCellElements
+                ?.filter(cell => cell.nativeElement.getAttribute('row') === r.toString())
+                .forEach(cell => {
+                    this.renderer.removeClass(cell?.nativeElement, 'type-cell-highlighted');
+                    this.renderer.addClass(cell?.nativeElement, 'type-cell-frozen');
+                });
         }
         else if (this.frozenRowIndex === r) {
             this.frozenRowIndex = -1;
-        }
 
+            this.typeCellElements
+                ?.filter(cell => cell.nativeElement.getAttribute('row') === r.toString() && cell.nativeElement.getAttribute('col') !== this.frozenColumnIndex.toString())
+                .forEach(cell => {
+                    this.renderer.removeClass(cell?.nativeElement, 'type-cell-frozen');
+                    this.renderer.addClass(cell?.nativeElement, 'type-cell-highlighted');
+                });
+        }
     }
 
     public freezeColumn(c: number): void {
         if (this.frozenColumnIndex !== c && this.frozenColumnIndex === -1) {
             this.frozenColumnIndex = c;
+
+            this.typeCellElements
+                ?.filter(cell => cell.nativeElement.getAttribute('col') === c.toString())
+                .forEach(cell => {
+                    this.renderer.removeClass(cell?.nativeElement, 'type-cell-highlighted');
+                    this.renderer.addClass(cell?.nativeElement, 'type-cell-frozen');
+                });
         }
         else if (this.frozenColumnIndex === c) {
             this.frozenColumnIndex = -1;
+
+            this.typeCellElements
+                ?.filter(cell => cell.nativeElement.getAttribute('col') === c.toString() && cell.nativeElement.getAttribute('row') !== this.frozenRowIndex.toString())
+                .forEach(cell => {
+                    this.renderer.removeClass(cell?.nativeElement, 'type-cell-frozen');
+                    this.renderer.addClass(cell?.nativeElement, 'type-cell-highlighted');
+                });
         }
     }
 
     public freezeColumnRow(r: number, c: number) {
-        if ((this.frozenColumnIndex === -1 && this.frozenRowIndex === -1) || (this.frozenColumnIndex === c && this.frozenRowIndex === r)) {
+        if ((this.frozenColumnIndex === -1 && this.frozenRowIndex === -1) ||
+            (this.frozenColumnIndex === c && this.frozenRowIndex === r)) {
             this.freezeColumn(c);
             this.freezeRow(r);
         }
@@ -94,29 +122,6 @@ export class TypeChartComponent implements OnInit {
     public generationSelected(selectedGeneration: number): void {
         this.selectedGenerationId = selectedGeneration;
         this.setDamageMultipliers();
-    }
-
-    private async setGenerations(): Promise<void> {
-        this.allGenerations = await this._generationService.getAllGenerationsDomain();
-
-        this.allGenerations.forEach(g => {
-            console.log(g.number);
-        });
-
-        // Set the pre-selected gen to the latest one.
-        this.selectedGenerationId = this.allGenerations
-            .map(g => g.number)
-            .sort((a, b) => b - a)[0];
-    }
-
-    private async setDamageMultipliers(): Promise<void> {
-        this.isLoading = true;
-        this.damageMultipliers = await this._typeService
-            .getDefensiveMultipliersForAllTypesByGeneration(this.selectedGenerationId)
-
-        const a = this.damageMultipliers;
-        this.types = this.damageMultipliers.flatMap(dm => dm.types);
-        this.isLoading = false;
     }
 
     public getDamageText(attacker: string, defender: string): string {
@@ -142,5 +147,28 @@ export class TypeChartComponent implements OnInit {
         }
 
         return ""
+    }
+
+    private async setGenerations(): Promise<void> {
+        this.allGenerations = await this._generationService.getAllGenerationsDomain();
+
+        this.allGenerations.forEach(g => {
+            console.log(g.number);
+        });
+
+        // Set the pre-selected gen to the latest one.
+        this.selectedGenerationId = this.allGenerations
+            .map(g => g.number)
+            .sort((a, b) => b - a)[0];
+    }
+
+    private async setDamageMultipliers(): Promise<void> {
+        this.isLoading = true;
+        this.damageMultipliers = await this._typeService
+            .getDefensiveMultipliersForAllTypesByGeneration(this.selectedGenerationId)
+
+        const a = this.damageMultipliers;
+        this.types = this.damageMultipliers.flatMap(dm => dm.types);
+        this.isLoading = false;
     }
 }
